bca884f503bc97fecc5a7f9449a6bb0dcc3a9863
Abijith Viswanath <abijithvv@gmail.com>
2023-11-24 20:08:57 -0500

    first commit of app

---
+++ src/main/java/co/abi/learning/jenkinstestapp/JenkinsTestAppApplication.java
@@ -0,0 +1,13 @@
+package co.abi.learning.jenkinstestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/abi/learning/jenkinstestapp/JenkinsTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.abi.learning.jenkinstestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
