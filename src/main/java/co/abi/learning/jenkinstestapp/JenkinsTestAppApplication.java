a1ea05f659b34633eb97c69747442b76d18503f0
Abijith Viswanath <abijithvv@gmail.com>
2023-11-24 21:32:44 -0500

    package changed to war

--- src/main/java/co/abi/learning/jenkinstestapp/JenkinsTestAppApplication.java
+++ src/main/java/co/abi/learning/jenkinstestapp/JenkinsTestAppApplication.java
@@ -2,12 +2,18 @@ package co.abi.learning.jenkinstestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestAppApplication {
+public class JenkinsTestAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestAppApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsTestAppApplication.class);
+    }
 }
